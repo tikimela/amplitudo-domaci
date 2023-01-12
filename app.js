d2b32b33badab90a7202c7b3c479232dfacd9e23
Mirela Abazovic <70277304+tikimela@users.noreply.github.com>
2023-01-12 02:31:34 +0100

    1

---
+++ app.js
@@ -0,0 +1,28 @@
+const film1 = {
+   odgledan : false,
+   naziv : "Enter the Void",
+   godina : 2009,
+   drzava : "France",
+   napomena : "Sex. Money. Power.",
+   glumci : ["Paz de la Huerta","Nathaniel Brown","Cyril Roy"],
+};
+
+const film2 = {
+   odgledan : false,
+   naziv : "Blade Runner 2049",
+   godina : 2017,
+   drzava : USA,
+   napomena : "The key to the future is finally unearthed.",
+   glumci : ["Ryan Gosling","Harrison Ford","Ana de Armas"],
+};
+
+const film3 = {
+   odgledan : false,
+   naziv : "The Lighthouse",
+   godina : 2019,
+   drzava : USA,
+   napomena : "There is enchantment in the light.",
+   glumci : ["Willem Dafoe","Robert Pattison","Valeriia Karman"],
+};
+
+const filmovi = [film1,film2,film3];
